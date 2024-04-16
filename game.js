15b1197d4c3b41d4ac9c8d82c9f1ed619b71622d
danny-gomes <dannnygomes@outlook.pt>
2024-04-16 17:47:56 +0100

    Finish basic game logic
    Finished base game logic, user inputs option, first character will be
    taken if its R will be rock etc.... Option is converted to upper case
    for correct comparasion. Added function chart_choice_to_long to then convert the single char
    into extended from to output in the alert message.

---
+++ game.js
@@ -0,0 +1,44 @@
+let player_choice = prompt("Rock, Paper, Scissors");
+player_choice = player_choice.charAt(0).toUpperCase();
+
+
+let pc_choice = Math.floor((Math.random() * 3) + 1);
+
+switch(pc_choice) {
+    case 1:
+        pc_choice = "R";
+        break;
+    case 2:
+        pc_choice = "P";
+        break;
+    case 3:
+        pc_choice = "S";
+        break;
+    default:
+        alert("Something went wrong in pc_choice attribution.");
+}
+
+if(player_choice === pc_choice){
+    alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nDraw.");
+} else if((player_choice === "R" && pc_choice === "S") || 
+          (player_choice === "P" && pc_choice === "R") ||
+          (player_choice === "S" && pc_choice === "P")){
+        alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nYou win.");
+} else {
+    alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nYou lose.");
+}
+
+function char_choice_to_long(char_choice){
+    switch(char_choice){
+        case "R":
+            return "Rock";
+        case "P":
+            return "Paper";
+        case "S":
+            return "Scissors";
+        default:
+            return "oops something went wrong converting char choice to long choice";
+    }
+}
+
+
