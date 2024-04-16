98ac77f0e8b85b79cc89c523ee2bea4b13935132
danny-gomes <dannnygomes@outlook.pt>
2024-04-16 18:37:05 +0100

    Add rounds, add validations for choices
    Made game a best of a certain number of rounds, added validations for
    the user choices so an invalid option can be chosen.

--- game.js
+++ game.js
@@ -1,44 +1,71 @@
-let player_choice = prompt("Rock, Paper, Scissors");
-player_choice = player_choice.charAt(0).toUpperCase();
+let player_score = 0;
+let pc_score = 0;
+let rounds = 3;
+let result;
 
+while(pc_score < rounds && player_score < rounds) {
+    result = game()
+    if(result === 1) {
+        player_score++;
+    } else if(result === 2) {
+        pc_score++;
+    }
 
-let pc_choice = Math.floor((Math.random() * 3) + 1);
-
-switch(pc_choice) {
-    case 1:
-        pc_choice = "R";
-        break;
-    case 2:
-        pc_choice = "P";
-        break;
-    case 3:
-        pc_choice = "S";
-        break;
-    default:
-        alert("Something went wrong in pc_choice attribution.");
+    alert("Player: " + player_score + " PC: " + pc_score);
 }
 
-if(player_choice === pc_choice){
-    alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nDraw.");
-} else if((player_choice === "R" && pc_choice === "S") || 
-          (player_choice === "P" && pc_choice === "R") ||
-          (player_choice === "S" && pc_choice === "P")){
-        alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nYou win.");
-} else {
-    alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nYou lose.");
-}
+function game() {
+    let player_choice = prompt("Rock, Paper, Scissors");
+    player_choice = player_choice.charAt(0).toUpperCase();
 
-function char_choice_to_long(char_choice){
-    switch(char_choice){
-        case "R":
-            return "Rock";
-        case "P":
-            return "Paper";
-        case "S":
-            return "Scissors";
+    while(player_choice.charAt(0).toUpperCase() !== "R" && player_choice.charAt(0).toUpperCase() !== "P" && player_choice.charAt(0).toUpperCase() !== "S") {
+        player_choice = prompt("That's not an option. Rock, Paper, Scissors");
+        player_choice = player_choice.charAt(0).toUpperCase();
+    }
+
+    let pc_choice = Math.floor((Math.random() * 3) + 1);
+
+    switch (pc_choice) {
+        case 1:
+            pc_choice = "R";
+            break;
+        case 2:
+            pc_choice = "P";
+            break;
+        case 3:
+            pc_choice = "S";
+            break;
         default:
-            return "oops something went wrong converting char choice to long choice";
+            alert("Something went wrong in pc_choice attribution.");
+    }
+
+    if (player_choice === pc_choice) {
+        alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nDraw.");
+        return 0;
+    } else if ((player_choice === "R" && pc_choice === "S") ||
+        (player_choice === "P" && pc_choice === "R") ||
+        (player_choice === "S" && pc_choice === "P")) {
+        alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nYou win.");
+        return 1;
+    } else {
+        alert(char_choice_to_long(player_choice) + "  Vs. " + char_choice_to_long(pc_choice) + "\nYou lose.");
+        return 2;
+    }
+
+    function char_choice_to_long(char_choice) {
+        switch (char_choice) {
+            case "R":
+                return "Rock";
+            case "P":
+                return "Paper";
+            case "S":
+                return "Scissors";
+            default:
+                return "oops something went wrong converting char choice to long choice";
+        }
     }
 }
+
+
 
 
